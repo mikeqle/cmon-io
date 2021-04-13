d4e3598a8eab7ffbd54ba635dfaeab054d294fe4
Mike Le <mike.le@live.com>
2021-04-13 11:40:00 -0700

    added starting backend files

---
+++ server/config/mongoose.config.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const db_name = "cmon";
+
+mongoose.connect("mongodb://localhost/" + db_name, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then( () => console.log("Successfully connected to the " + db_name + " database") )
+    .catch((err) => {
+        console.log("Something went wrong while connecting with the database " + db_name + ":");
+        console.log(err); // This makes sure the object is printed instead of the words [Object: Object]
+});
---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = 8000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+// setup mongodb 
+// this require statement is like copying and pasting the code from the file right here!
+require('./config/mongoose.config');
+
+// set up routes
+// require('./routes/user.routes')(app);
+
+app.listen(port, () => console.log("Listening on port: " + port));
