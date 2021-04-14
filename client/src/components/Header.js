682aaf248673531d43f7551cc91ae6794ccc21d0
Mike Le <mike.le@live.com>
2021-04-14 01:31:48 -0700

    preliminary RegisterForm working - still need polish

---
+++ client/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = (props) => {
+    return (
+        <div>
+            <h1>c'mon</h1>
+            <p>A common set of facts</p>
+        </div>
+    )
+};
+
+export default Header;
---
+++ client/src/components/RegisterForm.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { navigate } from '@reach/router';
+
+const RegisterForm = (props) => {
+    const [ username, setUsername ] = useState("");
+    const [ email, setEmail ] = useState("");
+    const [ password, setPassword ] = useState("");
+    const [ confirmPassword, setConfirmPassword ] = useState("");
+    
+    const [ errors, setErrors ] = useState({});
+
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+        
+        axios.post('http://localhost:8000/api/users/', {
+            username,
+            email,
+            password,
+            confirmPassword
+        })
+            .then((res) => {
+                if(res.data.errors) {
+                    console.log(res.data.errors);
+                    setErrors(res.data.errors);
+                }
+                else {
+                    navigate("/");
+                }
+            })
+            .catch((err) => console.log(err));
+    }
+
+    return(
+        <div>
+            <button>already have an account? Login here</button>
+            <h2>Signup with us</h2>
+            <form onSubmit={ onSubmitHandler }>
+                <div>
+                    <label>Username</label>
+                    <input type="text" name="username" value={ username } onChange={ e => setUsername(e.target.value) } />
+                    {/* handle error block below */}
+                </div>
+                <div>
+                    <label>Email</label>
+                    <input type="text" name="email" value={ email } onChange={ e => setEmail(e.target.value) } />
+                    {/* handle error block below */}
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input type="password" name="password" value={ password } onChange={ e => setPassword(e.target.value) } />
+                    {/* handle error block below */}
+                </div>
+                <div>
+                    <label>Confirm</label>
+                    <input type="password" name="confirmPassword" value={ confirmPassword } onChange={ e => setConfirmPassword(e.target.value) } />
+                    {/* handle error block below */}
+                </div>
+                <div>
+                    <button type="submit">Sign Up</button>
+                </div>
+            </form>
+        </div>
+    )
+};
+
+export default RegisterForm;
