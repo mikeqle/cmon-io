a0e456693312a428edd683c92356d204c8f86c6d
Mike Le <mike.le@live.com>
2021-04-18 22:20:27 -0700

    small edits to server.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const cookieParser = require('cookie-parser');
 
-require('dotenv').config()
+
+require('dotenv').config();
 const port = 8000;
 
 app.use(express.json());
